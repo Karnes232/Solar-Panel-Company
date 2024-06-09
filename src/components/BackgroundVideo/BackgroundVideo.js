09e9f9efedd8778f907d066b7ece00a693ed50bb
James Karnes <karnes.james@gmail.com>
2024-06-09 13:45:58 -0400

    added playsinline for video

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -3,7 +3,7 @@ import React from "react";
 const BackgroundVideo = ({ videoUrl }) => {
   return (
     <>
-      <video autoPlay loop muted id="backgroundVideo" className="brightness-75">
+      <video autoPlay loop muted playsinline id="backgroundVideo" className="brightness-75">
         <source src={videoUrl} type="video/mp4" />
       </video>
     </>
