032fb884f5f19341d70d50027344b3abd5a11b48
James Karnes <karnes.james@gmail.com>
2024-06-09 18:16:11 -0400

    added framer motion to icons

--- src/components/HeroComponent/IconSection.js
+++ src/components/HeroComponent/IconSection.js
@@ -1,36 +1,41 @@
 import React from "react";
-import { MdBatteryCharging80, MdOutlineAccessTime } from "react-icons/md";
+import { MdBatteryCharging80 } from "react-icons/md";
 import { SlEnergy } from "react-icons/sl";
 import IndividualIcon from "./IndividualIcon";
 import { graphql, useStaticQuery } from "gatsby";
 import { CiClock1 } from "react-icons/ci";
+import { motion } from "framer-motion";
 const IconSection = () => {
   const data = useStaticQuery(graphql`
     query IconQuery {
-      allContentfulAsset(filter: {title: {eq: "24-7"}}) {
+      allContentfulAsset(filter: { title: { eq: "24-7" } }) {
         nodes {
           url
           title
         }
       }
-    }`);
-    console.log(data.allContentfulAsset.nodes[0].url)
+    }
+  `);
+  console.log(data.allContentfulAsset.nodes[0].url);
   return (
     <div className="z-50 !absolute bottom-[5rem] lg:bottom-[8rem] xl:bottom-[10rem] 2xl:bottom-[12rem] w-screen flex items-center justify-center">
-      <div className="flex w-2/3 justify-between lg:max-w-md 2xl:max-w-lg">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{
+          duration: 3,
+          delay: 0.3,
+        }}
+        className="flex w-2/3 justify-between lg:max-w-md 2xl:max-w-lg"
+      >
         <IndividualIcon
           icon={<MdBatteryCharging80 />}
           title="Battery Energy Storage"
         />
-        <IndividualIcon
-          icon={<CiClock1 />}
-          title="Outage Protection"
-        />
-        <IndividualIcon
-          icon={<SlEnergy />}
-          title="Energy Independence"
-        />
-      </div>
+        <IndividualIcon icon={<CiClock1 />} title="Outage Protection" />
+        <IndividualIcon icon={<SlEnergy />} title="Energy Independence" />
+      </motion.div>
     </div>
   );
 };
---
+++ src/pages/about/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const Index = () => {
+  return <Layout>Hello World</Layout>;
+};
+
+export default Index;
